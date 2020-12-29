72c81113c5229ee2e70c3d237320188a9b634748
Woo-yeong Kim <kimwy1997@gmail.com>
2020-12-30 01:37:37 +0900

    [CREATE] URI to create admin

---
+++ controllers/adminController.js
@@ -0,0 +1,50 @@
+const passport = require("passport");
+const { Admin, Video } = require("../models");
+// const crypto = require("../modules/crypto");
+const bcrypt = require("bcrypt");
+const jwt = require("../middlewares/jwt");
+const responseMessage = require("../modules/responseMessage");
+const statusCode = require("../modules/statusCode");
+const util = require("../modules/util");
+
+module.exports = {
+  createAdmin: async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE);
+    }
+
+    try {
+      const usernameExists = await Admin.findOne({ where: username });
+      if (usernameExists) {
+        return res
+          .status(statusCode.CONFLICT)
+          .send(statusCode.CONFLICT, responseMessage.ALREADY_USERNAME_ADMIN);
+      }
+      const { salt, hashed } = await encrpyt(password);
+      const admin = Admin.create({ username, password: hashed, salt });
+      console.log(admin);
+      res
+        .status(statusCode.OK)
+        .send(
+          util.success(
+            statusCode.OK,
+            responseMessage.CREATE_ADMIN_SUCCESS,
+            admin,
+          ),
+        );
+    } catch (error) {
+      console.log(error);
+      res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR,
+          ),
+        );
+    }
+  },
+};
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const adminRouter = express.Router();
 
+// Create an Admin
+adminRouter.post("/");
+
+// Read all Admins
 adminRouter.get("/");
+
+// Read one Admin
+adminRouter.get("/:adminId");
+
+// Update Admin
+adminRouter.put("/:adminId");
+
+// Delete Admin
+adminRouter.delete("/:adminId");
 
 module.exports = adminRouter;
