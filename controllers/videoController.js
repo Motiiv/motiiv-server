951d6e0926292da6abbe3785c0e7e91f7f82759e
jeongjae <rchmas@gmail.com>
2020-12-31 21:22:06 +0900

    add/delete like

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,40 +6,38 @@ const { Section, Tag, User, Video_Section, Video_Tag, Video, Like, Save, Workspa
 const Op = sequelize.Op;
 
 module.exports = {
+
     readAllPost: async (req, res) => {
         const filters = req.query.filters;
         try {
             const video = await Video.findAll({
-                group: "id",
-                attributes: [
-                    "id",
-                    "videoUrl",
-                    "title",
-                    "description",
-                    "thumbnailImageUrl",
-                    "viewCount",
-                    "videoLength",
-                    "channelName",
-                    [sequelize, fn("COUNT", "LikedVideos.VideoLike.VideoId"), "likeCnt"],
-                ],
-                include: [
-                    {
-                        model: User,
-                        as: "VideoLikers",
-                        attributes: [],
-                        through: { attributes: [] },
-                    },
-                ],
+                group: 'id',
+                attributes: ['id', 'videoUrl', 'title', 'description', 'thumbnailImageUrl', 'viewCount', 'videoLength', 'channelName',
+                    [sequelize, fn("COUNT", "LikedVideos.VideoLike.VideoId"), 'likeCnt']],
+                include: [{
+                    model: User,
+                    as: 'VideoLikers',
+                    attributes: [],
+                    through: { attributes: [] }
+                }]
             });
 
             return res
                 .status(sc.OK)
-                .send(ut(success(sc.OK, rm.GET_ALL_POST_SUCCESS, video)));
+                .send(
+                    ut(success(
+                        sc.OK,
+                        rm.GET_ALL_POST_SUCCESS,
+                        video
+                    )))
         } catch (err) {
             console.log(err);
             return res
-                .status(sc.INTERNAL_SERVER_ERROR)
-                .send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.GET_ALL_POST_FAIL));
+                .status(sc.INTERNAL_SERVER_ERROR).
+                send(ut.fail(
+                    sc.INTERNAL_SERVER_ERROR,
+                    rm.GET_ALL_POST_FAIL
+                ));
         }
     },
 
@@ -88,6 +86,9 @@ module.exports = {
 
         try {
             const like = await Like.create({ VideoId: video, UserId: user });
+
+            // 중복 처리 추가
+
             return res
                 .status(sc.OK)
                 .send(ut.success(sc.OK, rm.POST_VIDEO_LIKE_SUCCESS, like));
--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -37,9 +37,6 @@ module.exports = {
   GET_ALL_ADMINS_FAIL: "모든 관리자 조회 실패",
   GET_ONE_ADMIN_SUCCESS: "관리자 조회 성공",
   GET_ONE_ADMIN_FAIL: "관리자 조회 실패",
-  UPDATE_ADMIN_USERNAME_SUCCESS: "관리자 이름 변경 성공",
-  UPDATE_ADMIN_PASSWORD_SUCCESS: "관리자 비밀번호 변경 성공",
-  SAME_ADMIN_USERNAME: "기존 관리자 이름과 동일합니다",
   DELETE_ADMIN_SUCCESS: "관리자 삭제 성공",
   DELETE_ADMIN_FAIL: "관리자 삭제 실패",
 
@@ -67,10 +64,7 @@ module.exports = {
   POST_VIDEO_LIKE_SUCCESS: "댓글 좋아요 성공",
   POST_VIDEO_LIKE_FAIL: "댓글 좋아요 실패",
   DELETE_VIDEO_LIKE_SUCCESS: "댓글 좋아요 취소 성공",
-  DELETE_VIDEO_LIKE_FAIL: "댓글 좋아요 실패",
+  DELETE_VIDEO_LIKE_FAIL: "댓글 좋아요 실패"
 
 
-  /* Main_Video */
-  GET_ALL_POST_SUCCESS: "비디오 불러오기 성공",
-  GET_ALL_POST_FAIL: "비디오 불러오기 실패",
 };
