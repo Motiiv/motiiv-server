e82b6bb236769512e643ded529f0dfca51ee2276
Woo-yeong Kim <kimwy1997@gmail.com>
2021-01-09 03:23:58 +0900

    [UPDATE] set sameSite option to 'None' for cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -189,7 +189,7 @@ module.exports = {
       const { accessToken } = await jwt.sign(user);
       res
         .status(statusCode.OK)
-        .cookie("userToken", accessToken, { sameSite: "none" })
+        .cookie("userToken", accessToken, { sameSite: "None" })
         .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
     } catch (error) {
       console.log(error);
