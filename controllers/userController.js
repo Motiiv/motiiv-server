81e4aeda0e491346d68e2820eb5b8c77e6175785
Woo-yeong Kim <kimwy1997@gmail.com>
2021-01-09 03:00:39 +0900

    Fix postImageUrl field in postModel

--- controllers/userController.js
+++ controllers/userController.js
@@ -190,8 +190,6 @@ module.exports = {
       res
         .status(statusCode.OK)
         .cookie("userToken", accessToken)
-        .setHeader("Access-Control-Allow-Origin", "localhost:3000")
-        .setHeader("Access-Control-Allow-Credentials", "true")
         .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
     } catch (error) {
       console.log(error);
