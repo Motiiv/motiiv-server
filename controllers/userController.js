7e939c4e9ec6f9af902cb809387360e1660666fa
Woo-yeong Kim <kimwy1997@gmail.com>
2021-01-09 02:19:50 +0900

    [UPDATE] remove send temporarily for cors

--- controllers/userController.js
+++ controllers/userController.js
@@ -187,10 +187,8 @@ module.exports = {
           );
       }
       const { accessToken } = await jwt.sign(user);
-      res
-        .status(statusCode.OK)
-        .cookie("userToken", accessToken)
-        .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
+      res.status(statusCode.OK).cookie("userToken", accessToken);
+      // .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
     } catch (error) {
       console.log(error);
       res
